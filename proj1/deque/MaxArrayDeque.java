eaa4001b04b74f356283dd3c99482e73a6dda145
chegan <Chegangan123@gmail.com>
2024-11-23 14:58:33 +0800

    project1 comparator version1

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -20,10 +20,17 @@ public class MaxArrayDeque<T> extends ArrayDeque<T> {
     }
     
     private T compare(Comparator<T> c) {
-        /*for (int i = 0; i < ; i++) {
-            
-        }*/
-        return null;
+        if(this.size == 0){
+            return null;
+        }
+        T max = this.get(0);
+        for (int i = 0; i < this.size(); i++) {
+            int flag = c.compare(max,get(i));
+            if(flag < 0){
+                max = this.get(i);
+            }
+        }
+        return max;
     }
 
     private static class MyComparator implements Comparator<Integer> {
@@ -31,6 +38,9 @@ public class MaxArrayDeque<T> extends ArrayDeque<T> {
         /**
          * @param o1 the first object to be compared. 
          * @param o2 the second object to be compared.
+         * 如果o1大于o2,那么返回正数
+         * 如果o1小于o2,那么返回负数
+         * 如果o1等于o2,返回0
          * @return
          */
         @Override
@@ -39,7 +49,48 @@ public class MaxArrayDeque<T> extends ArrayDeque<T> {
         }
     }
 
-    public Comparator<Integer> getComparator() {
+    public static Comparator<Integer> getComparator() {
         return new MyComparator();
+    }
+    
+    
+    private static class MyComparator2 implements Comparator<String> {
+
+        /**
+         * @param o1 the first object to be compared. 
+         * @param o2 the second object to be compared.
+         * @return
+         */
+        @Override
+        public int compare(String o1, String o2) {
+            return o1.length() - o2.length();
+        }
+    }
+
+    public static Comparator<String> getComparator2() {
+        return new MyComparator2();
+    }
+
+    public static void main(String[] args) {
+        Comparator<Integer> c = MaxArrayDeque.getComparator();
+        Comparator<String> c2 = MaxArrayDeque.getComparator2();
+        MaxArrayDeque<Integer> maxArrayDeque = new MaxArrayDeque<Integer>(c);
+        for (int i = 0; i < 20; i++) {
+            maxArrayDeque.addFirst(i);
+        }
+        MaxArrayDeque<String> maxArrayDeque1 = new MaxArrayDeque<>(c2);
+        for (int i = 0; i < 20; i++) {
+            String str = "";
+            for (int j = 0; j < i; j++) {
+                str += "a";
+            }
+            maxArrayDeque1.addFirst(str);
+        }
+
+        Integer a = maxArrayDeque.max();
+        System.out.println(a);
+
+        String b = maxArrayDeque1.max();
+        System.out.println(b);
     }
 }
